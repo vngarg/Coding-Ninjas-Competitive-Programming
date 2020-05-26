8d0ac9c318e0fa0d6947ebbab98fdfdacaf44ae7
vngarg <sngargrsd@gmail.com>
2020-05-27 02:00:50 +0530

    Merge Sort Added

--- Sorting Algorithms/MergeSort.cpp
+++ Sorting Algorithms/MergeSort.cpp
@@ -2,33 +2,42 @@
 
 using namespace std;
 
-void MergeFunction(int arr[] , int start1 , int end1 , int start2 , int end2 ) {
-    int *p1 = &start1, *p2 = &start2 , finalarr[end1 + end2 + 2] , index = 0 ;
-
-    while(p1 || p2) {
-        if(p1 >= p2) {
-            finalarr[index] = *p1;
-            p1 = p1 + 1 ;
-        } else {
-            finalarr[index] = *p2;
-            p2 = p2 + 1;
-        }
-        ++index;
-    }
-
-    for( index = 0 ; index < end1+end2+2 ; ++index)
-        cout<<finalarr[index]<<" ";
+void merge(int *Arr, int start, int mid, int end) {
+	int temp[end - start + 1];
+	int i = start, j = mid+1, k = 0;
+	while(i <= mid && j <= end) {
+		if(Arr[i] <= Arr[j]) {
+			temp[k] = Arr[i];
+			k += 1; i += 1;
+		}
+		else {
+			temp[k] = Arr[j];
+			k += 1; j += 1;
+		}
+	}
+	while(i <= mid) {
+		temp[k] = Arr[i];
+		k += 1; i += 1;
+	}
+	while(j <= end) {
+		temp[k] = Arr[j];
+		k += 1; j += 1;
+	}
+	for(i = start; i <= end; i += 1) {
+		Arr[i] = temp[i - start];
+	}
 }
 
-void SplitFunction(int arr[] , int start , int end) {
-    int mid;
-    mid = (start + end) / 2 ;
-    if(start != end) {
-        SplitFunction(arr , start , mid ) ; 
-        SplitFunction(arr , mid + 1 , end);
-        MergeFunction(arr , start , mid , mid + 1 , end );
-    } 
+void mergeSort(int *Arr, int start, int end) {
+    
+	if(start < end) {
+		int mid = (start + end) / 2;
+		mergeSort(Arr, start, mid);
+		mergeSort(Arr, mid+1, end);
+		merge(Arr, start, mid, end);
+	}
 }
+
 
 int main() {
     int size;
@@ -39,7 +48,14 @@ int main() {
     for(i=0 ; i< size ; ++i )
         cin>>arr[i];
 
-    SplitFunction(arr , 0 , size-1);
+    mergeSort(arr , 0 , size-1);
+
+    cout<<"Sorted Array id: ";
+
+    for(i=0;i<size;++i)
+        cout<<arr[i]<<" ";
+
+    cout<<endl;
 
     return 0;
 }
