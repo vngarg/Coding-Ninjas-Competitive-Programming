cc97f7d338806f24193e80b73ce87885ad269365
vngarg <sngargrsd@gmail.com>
2020-08-05 13:10:16 +0530

    Update Segment Tree

--- Segment Tree/SegmentTree_Operations.cpp
+++ Segment Tree/SegmentTree_Operations.cpp
@@ -5,7 +5,8 @@ void printChoices() {
     cout<<"SELECT YOUR CHOICE:"<<endl;
     cout<<"1. GENERTATE TREE"<<endl;
     cout<<"2. PRINT SEGMENT TREE"<<endl;
-    cout<<"3. EXIT"<<endl;
+    cout<<"3. UPDATE TREE"<<endl;
+    cout<<"4. EXIT"<<endl;
 }
 
 void generateTree(int* arr, int* tree, int start, int end, int treeNode) {
@@ -21,16 +22,33 @@ void generateTree(int* arr, int* tree, int start, int end, int treeNode) {
     return ;
 }
 
+void updateTree(int* arr, int* tree, int start, int end, int treeNode, int index, int value) {
+    if(start == end) {
+        arr[index] = value;
+        tree[treeNode] = value;
+        return;
+    }
+
+    int mid = (start + end) / 2;
+    if(index <= mid) {
+        updateTree(arr, tree, start, mid, 2*treeNode, index, value);
+    }  else {
+        updateTree(arr, tree, mid + 1, end, 2*treeNode + 1, index, value);
+    }
+
+    tree[treeNode] = tree[2*treeNode] + tree[2*treeNode + 1];
+}
+
 void printTree(int* tree, int n) {
     cout<<"TREE:"<<endl;
-    for(int i=0;i<n;++i)
+    for(int i=1;i<n;++i)
         cout<<tree[i]<<" ";
-    cout<<endl;
+    cout<<endl<<endl;
 }
 
 int main() {
     cout<<"Enter the number of values"<<endl;
-    int n;
+    int n, index, value;
     cin>>n;
     cout<<"Enter the values"<<endl;
     int arr[n], tree[2*n];
@@ -45,12 +63,20 @@ int main() {
         switch(choice) {
             case 1:
                 generateTree(arr, tree, 0, n-1, 1);
-                cout<<"TREE GENERATED"<<endl;
+                cout<<"TREE GENERATED"<<endl<<endl;
                 break;
             case 2:
                 printTree(tree, 2*n);
                 break;
             case 3:
+                cout<<"ENTER THE ARRAY INDEX TO BE UPDATED:"<<endl;
+                cin>>index;
+                cout<<"ENTER NEW VALUE:"<<endl;
+                cin>>value;
+                updateTree(arr, tree, 0, n-1, 1, index, value);
+                printTree(tree, 2*n);
+                break;
+            case 4:
                 return 0;
         }
     }
